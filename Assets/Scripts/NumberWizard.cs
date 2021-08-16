3574ac78cdc60709062a10c8160566f840f96237
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-08-16 09:43:28 -0400

    Add files via upload

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int min;
+    int max;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
+
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number in your head between " + min + " and " + max + ". Don't tell me what it is...");
+        Debug.Log("Is your number higher or lower than " + guess + "?");
+        Debug.Log("Up = Higher | Down = Lower | Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log(guess + " was the correct guess! Restarting Game...");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than " + guess + "?");
+    }
+
+}
---
+++ Assets/Scripts/NumberWizardUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI count;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    int guessCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        if (max != guess)
+        {
+            min = guess + 1;
+            NextGuess();
+        }
+    }
+
+    public void OnPressLower()
+    {
+        if (min != guess)
+        {
+            max = guess - 1;
+            NextGuess();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+
+        guessCount++;
+        count.text = guessCount.ToString();
+    }
+}
